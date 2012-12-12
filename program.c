db66c1a986584aff11ca7100192096f1662305b2
Sam Panzer <panzers@seas.upenn.edu>
2012-12-12 02:31:22 -0500

    Abstracted the string which should be ignored

--- program.c
+++ program.c
@@ -5,8 +5,9 @@ int main(int argc, char **argv) {
   int i = 1;
 
   printf("Adjusted argument list was \"");
+  char *to_remove = argv[1];
   for (; i < argc; ++i)
-    if (strcmp(argv[1], argv[i]))
+    if (strcmp(to_remove, argv[i]))
       printf("%s ", argv[i]);
   printf("\".\n");
   return 0;
