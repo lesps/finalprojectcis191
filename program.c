c99a63ecb82a1dbbfbb82bd172780d876d40a8d6
Sam Panzer <panzers@seas.upenn.edu>
2012-12-12 02:35:30 -0500

    Complain and quit if there are no arguments given

--- program.c
+++ program.c
@@ -4,6 +4,12 @@
 int main(int argc, char **argv) {
   int i = 1;
 
+  /* If no arguments are given, complain and exit. */
+  if (argc < 2) {
+    fprintf(stderr, "Error: no arguments specified.\n");
+    return 1;
+  }
+
   printf("Adjusted argument list was \"");
   char *to_remove = argv[1];
   for (; i < argc; ++i)
